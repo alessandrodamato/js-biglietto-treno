d67b17c491d71a90f7080a3ba043db1663218239
alessandrodamato <alessandro042004@gmail.com>
2024-02-20 15:34:30 +0100

    [add] price in console

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// dati utente
+const km = prompt('Quanti km vuoi percorrere?');
+const age = prompt('Quanti anni hai?');
+
+console.log(km, age);
+
+// calcolo prezzo (senza sconti)
+let price = km * 0.21;
+
+console.log('Prezzo base: €', price);
+
+if (age < 18) {
+  price = price - price * 20 / 100;
+  console.log('Prezzo con sconto minorenni 20%: €', price.toFixed(2));
+} else if (age > 65) {
+  price = price - price * 40 / 100;
+  console.log('Prezzo con sconto over 65 40%: €', price.toFixed(2));
+} else {
+  console.log('Prezzo base: €', price.toFixed(2));
+}
